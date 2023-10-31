219c3daf9576a479c22716d18659d5ca93d33f96
layla <lmassey2021@gmail,com>
2023-10-31 16:22:49 -0500

    fix url againnnnnn omg

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -150,7 +150,7 @@ export default {
         return [
           '<a href="https://twitter.com/pilatesdev" target="_blank">pilatesdev @ twitter</a>',
           ' ',
-          '<a href="https://www.linkedin.com/in/laylacodes" target="_blank">layla massey @ linkedin</a>',
+          '<a href="https://linkedin.com/in/layla-massey-18b2ab11as">layla massey @ linkedin</a>',
           ' ',
           '<a href="https://instagram.com/pilatesdev" target="_blank">pilatesdev @ instagram</a>',
           ' ',
